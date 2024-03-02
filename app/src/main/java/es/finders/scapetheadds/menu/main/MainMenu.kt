c9dc4c16dc9e4ce06b6133ac6330a4a5fca02fe0
DanielS-source <daniel.silva.iglesias@udc.es>
2024-03-02 18:15:22 +0100

    Added Login and Nickname page. Also boilerplate for the other screens

--- app/src/main/java/es/finders/scapetheadds/menu/main/MainMenu.kt
+++ app/src/main/java/es/finders/scapetheadds/menu/main/MainMenu.kt
@@ -1,2 +1,43 @@
 package es.finders.scapetheadds.menu.main
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import es.finders.scapetheadds.ui.theme.ScapeTheAddsTheme
+import es.finders.scapetheadds.ui.utils.BasicBackground
+
+class MainMenu : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ScapeTheAddsTheme {
+                MainMenuScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainMenuScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier,
+        contentAlignment = Alignment.Center,
+    ) {
+        BasicBackground(modifier.fillMaxSize())
+        // ComponentLayout(modifier.fillMaxSize())
+    }
+}
+
+@Preview(showBackground = false, showSystemUi = true)
+@Composable
+private fun MainMenuScreenPreview() {
+    ScapeTheAddsTheme {
+        MainMenuScreen()
+    }
+}
