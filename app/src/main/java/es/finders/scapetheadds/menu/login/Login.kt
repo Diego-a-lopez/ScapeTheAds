1e8d16f5c2219b23219b2dbe41b5669a6e8ac7af
DanielS-source <daniel.silva.iglesias@udc.es>
2024-03-02 16:14:59 +0100

    Added i18n (Spanish/English), login ui, and several assets

--- app/src/main/java/es/finders/scapetheadds/menu/login/Login.kt
+++ app/src/main/java/es/finders/scapetheadds/menu/login/Login.kt
@@ -1,2 +1,93 @@
 package es.finders.scapetheadds.menu.login
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import es.finders.scapetheadds.R
+import es.finders.scapetheadds.ui.theme.ScapeTheAddsTheme
+import es.finders.scapetheadds.ui.utils.BasicBackground
+import es.finders.scapetheadds.ui.utils.Logo
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ScapeTheAddsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier,
+        contentAlignment = Alignment.Center,
+    ) {
+        BasicBackground(modifier.fillMaxSize())
+        LoginLayout(modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun LoginLayout(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Logo()
+        Spacer(Modifier.size(30.dp))
+        NextButton({}, Modifier.padding(top = 5.dp, bottom = 20.dp))
+    }
+}
+
+@Composable
+fun NextButton(onClick: () -> Unit, modifier: Modifier) {
+    Button(
+        onClick = { onClick() },
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(R.string.next),
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            fontSize = 10.em,
+        )
+    }
+}
+
+@Preview(showBackground = false, showSystemUi = true)
+@Composable
+private fun BirthdayCardPreview() {
+    ScapeTheAddsTheme {
+        LoginScreen()
+    }
+}
