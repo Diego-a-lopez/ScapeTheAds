7b7b051270603c57fb80452c3baeb9c56e9616b3
Diego-a-lopez <71869193+Diego-a-lopez@users.noreply.github.com>
2024-03-07 14:30:40 +0100

    Home Screen and LeaderBoard V2
    -Make the screens theme compliant
    
    -cleaned the code into separate funtions
    
    - implemented the outlined text functionlity

--- app/src/main/java/es/finders/scapetheadds/menu/home/Home.kt
+++ app/src/main/java/es/finders/scapetheadds/menu/home/Home.kt
@@ -3,24 +3,28 @@ package es.finders.scapetheadds.menu.home
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import es.finders.scapetheadds.ui.theme.RedPrimary
 import es.finders.scapetheadds.ui.theme.ScapeTheAddsTheme
+import es.finders.scapetheadds.ui.utils.BasicBackground
+import es.finders.scapetheadds.ui.utils.Logo
 
 class Home : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,88 +39,89 @@ class Home : ComponentActivity() {
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    val buttonHeight = 100.dp // Adjust button height as needed
-    val buttonWidth = buttonHeight * 5 // Width is 5 times the height
-    val buttonModifier = Modifier
-        .padding(16.dp)
-        .height(buttonHeight)
-        .width(buttonWidth)
-
-    val buttonColors = ButtonDefaults.buttonColors(
-        contentColor = MaterialTheme.colorScheme.onPrimary
-    )
-
-    val buttonShape = MaterialTheme.shapes.medium
-
-    Column(
-        modifier = modifier
-            .fillMaxSize()
+    Box(
+        modifier,
+        contentAlignment = Alignment.Center,
     ) {
-        // Text on the upper half
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = "Scape The Adds",
-                modifier = Modifier.padding(horizontal = 16.dp),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.displaySmall
+        BasicBackground(modifier.fillMaxSize())
+        HomeLayout(modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun HomeLayout(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlineTextSection("Scape The Adds")
+        Logo()
+        BottomButtonsSection()
+    }
+}
+@Composable
+fun OutlineTextSection(text: String) {
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text ,
+            modifier = Modifier.padding(horizontal = 16.dp),
+            textAlign = TextAlign.Center,
+            style = LocalTextStyle.current.merge(
+                TextStyle(
+                    color = RedPrimary, // Set to primary color
+                    fontSize = MaterialTheme.typography.displayMedium.fontSize,
+                )
+            ),
+        )
+        Text(
+            text = text,
+            modifier = Modifier.padding(horizontal = 16.dp),
+            textAlign = TextAlign.Center,
+            style = LocalTextStyle.current.merge(
+                TextStyle(
+                    color = Color.Black, // Set to primary color
+                    fontSize = MaterialTheme.typography.displayMedium.fontSize,
+                    drawStyle = Stroke(width = 5f, join = StrokeJoin.Round)
+                )
             )
-        }
+        )
+    }
+}
+@Composable
+fun BottomButtonsSection() {
+    val buttonModifier = Modifier
+        .padding(vertical = 8.dp)
+        .fillMaxWidth()
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+    ) {
+        ButtonItem(text = "Jugar", {}, buttonModifier)
+        ButtonItem(text = "Selección de Niveles", {}, buttonModifier)
+        ButtonItem(text = "Puntuación Local", {}, buttonModifier)
+        ButtonItem(text = "Puntuación Global", {}, buttonModifier)
+    }
+}
 
-        // Placeholder app icon
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f),
-            contentAlignment = Alignment.Center
-        ) {
-            // Placeholder app icon goes here
-            Text(text = "App Icon Placeholder")
-        }
+@Composable
+fun ButtonItem(text: String, onClick: () -> Unit, modifier: Modifier) {
 
-        // Bottom half with buttons
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp)
-        ) {
-            Button(
-                onClick = { /* Handle button click */ },
-                modifier = buttonModifier,
-                shape = buttonShape,
-                colors = buttonColors
-            ) {
-                Text(text = "Jugar")
-            }
-            Button(
-                onClick = { /* Handle button click */ },
-                modifier = buttonModifier,
-                shape = buttonShape,
-                colors = buttonColors
-            ) {
-                Text(text = "Selección de Niveles")
-            }
-            Button(
-                onClick = { /* Handle button click */ },
-                modifier = buttonModifier,
-                shape = buttonShape,
-                colors = buttonColors
-            ) {
-                Text(text = "Puntuación Local")
-            }
-            Button(
-                onClick = { /* Handle button click */ },
-                modifier = buttonModifier,
-                shape = buttonShape,
-                colors = buttonColors
-            ) {
-                Text(text = "Puntuación Global")
-            }
-        }
+
+    Button(
+        onClick = { onClick() },
+        modifier = modifier,
+        shape = RoundedCornerShape(35),
+        contentPadding = PaddingValues(top = 10.dp, bottom = 10.dp),
+        border = BorderStroke(1.dp, Color.Black),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = RedPrimary
+        )) {
+        Text(text = text)
     }
 }
 
--- app/src/main/java/es/finders/scapetheadds/menu/leaderborad/Leaderboard.kt
+++ app/src/main/java/es/finders/scapetheadds/menu/leaderborad/Leaderboard.kt
@@ -1,31 +1,33 @@
-package es.finders.scapetheadds.menu.leaderborad
+package es.finders.scapetheadds.menu.leaderboard
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import es.finders.scapetheadds.menu.login.LoginLayout
+import es.finders.scapetheadds.ui.theme.RedPrimary
 import es.finders.scapetheadds.ui.theme.ScapeTheAddsTheme
+import es.finders.scapetheadds.ui.utils.BasicBackground
 
 class Leaderboard : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,14 +41,24 @@ class Leaderboard : ComponentActivity() {
 }
 
 @Composable
-fun LeaderboardScreen() {
+fun LeaderboardScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier,
+        contentAlignment = Alignment.Center,
+    ) {
+        BasicBackground(modifier.fillMaxSize())
+        LeaderboardLayout(modifier.fillMaxSize())
+    }
+}
+@Composable
+fun LeaderboardLayout(modifier: Modifier = Modifier) {
     // Variables for styling
     val textTitle = "Puntuación Global"
     val titleTextColor = MaterialTheme.colorScheme.onPrimary
-    val titleTextOutlineColor = MaterialTheme.colorScheme.onTertiary
+    val titleTextOutlineColor = MaterialTheme.colorScheme.onSecondary
 
-    val containerColor = MaterialTheme.colorScheme.onSecondary
-    val containerOutlineColor = MaterialTheme.colorScheme.onTertiary
+    val containerColor = MaterialTheme.colorScheme.surface
+    val containerOutlineColor = MaterialTheme.colorScheme.onSurface
     val containerElevation = 4.dp
 
     Column(
@@ -56,77 +68,101 @@ fun LeaderboardScreen() {
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         // Upper Left Arrow to go back to home screen
-        Icon(
-            imageVector = Icons.Default.ArrowBack,
-            contentDescription = "Back",
-            modifier = Modifier
-                .size(48.dp)
-                .clickable {
-                    // Handle navigation back to home screen
-                }
-        )
+        BackButton()
 
-        // Top Text Field "Puntuación Global"
-        Text(
-            text = textTitle,
-            style = MaterialTheme.typography.displaySmall
-                .copy(color = titleTextColor),
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
+        OutlineTextSection("Puntuación Global")
 
         // Container for user info rows
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-        ) {
-            // Each row wrapped in a Surface container
-            Surface(
-                modifier = Modifier.fillMaxWidth(),
-                color = containerColor,
-                shape = MaterialTheme.shapes.medium,
-                border = BorderStroke(1.dp, containerOutlineColor),
-            ) {
-                UserInfoRow("User Name", "Score", textColor = containerOutlineColor)
-            }
-            Surface(
-                modifier = Modifier.fillMaxWidth(),
-                color = containerColor,
-                shape = MaterialTheme.shapes.medium,
-                border = BorderStroke(1.dp, containerOutlineColor),
-            ) {
-                UserInfoRow("John Doe", "100", textColor = containerOutlineColor)
-            }
-            // Add more Surface containers for other users as needed
-        }
+        UserInfoContainer(containerColor, containerOutlineColor)
     }
 }
+@Composable
+fun OutlineTextSection(text: String) {
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            modifier = Modifier.padding(horizontal = 16.dp),
+            textAlign = TextAlign.Center,
+            style = LocalTextStyle.current.merge(
+                TextStyle(
+                    color = RedPrimary, // Set to primary color
+                    fontSize = MaterialTheme.typography.displaySmall.fontSize,
+                )
+            ),
+        )
+        Text(
+            text = text,
+            modifier = Modifier.padding(horizontal = 16.dp),
+            textAlign = TextAlign.Center,
+            style = LocalTextStyle.current.merge(
+                TextStyle(
+                    color = Color.Black, // Set to primary color
+                    fontSize = MaterialTheme.typography.displaySmall.fontSize,
+                    drawStyle = Stroke(width = 4f, join = StrokeJoin.Round)
+                )
+            )
+        )
+    }
+}
+@Composable
+fun BackButton() {
+    Icon(
+        imageVector = Icons.Default.ArrowBack,
+        contentDescription = "Back",
+        modifier = Modifier
+            .size(48.dp)
+            .clickable {
+                // Handle navigation back to home screen
+            }
+    )
+}
 
-
+@Composable
+fun UserInfoContainer(containerColor: Color, containerOutlineColor: Color) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        // Each row wrapped in a Surface container
+        UserInfoRow("User Name", "Score", containerOutlineColor)
+        Spacer(modifier = Modifier.height(16.dp)) // Adding space between user rows
+        UserInfoRow("John Doe", "100", containerOutlineColor)
+        Spacer(modifier = Modifier.height(16.dp)) // Adding space between user rows
+        // Add more users as needed
+    }
+}
 @Composable
 fun UserInfoRow(
     userName: String,
     score: String,
-    textColor: androidx.compose.ui.graphics.Color
+    textColor: Color
 ) {
     // Variables for styling
     val paddingValue = 16.dp
     val scorePaddingValue = 8.dp
 
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(paddingValue),
-        verticalAlignment = Alignment.CenterVertically
+    Surface(
+        modifier = Modifier.fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium,
+        border = BorderStroke(3.dp, Color.Black),
     ) {
-        Text(text = userName, color = textColor)
-        Spacer(modifier = Modifier.weight(1f))
-        Text(text = score, color = textColor)
-        Spacer(modifier = Modifier.width(scorePaddingValue))
-        Text(text = "pto", color = textColor)
+        Row(
+            modifier = Modifier.padding(paddingValue),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(text = userName, color = textColor)
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = score, color = textColor)
+            Spacer(modifier = Modifier.width(scorePaddingValue))
+            Text(text = "pto", color = textColor)
+        }
     }
 }
 
-@Preview(showBackground = false, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 private fun LeaderboardScreenPreview() {
     ScapeTheAddsTheme {
